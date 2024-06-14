ee13b33506ef2895efca79cb122fc335ce9f14b7
王鑫 <wb_wangxin06@kuaishou.com>
2024-06-14 10:27:47 +0800

    用户管理模块 interceptor 修改信息接口

--- meeting_room_booking_system_backend/src/user/user.controller.ts
+++ meeting_room_booking_system_backend/src/user/user.controller.ts
@@ -1,8 +1,13 @@
 import { Controller, Inject, Get, Post, Body, Query } from '@nestjs/common';
 import { UserService } from './user.service';
 import { RegisterUserDto } from './dto/register.dto';
+import { RequireLogin, UserInfo } from '@/custom.decorator';
 import { EmailService } from 'src/email/email.service';
 import { RedisService } from 'src/redis/redis.service';
+import { UserDetailVo } from './vo/user-info.vo';
+import { UpdateUserPasswordDto } from './dto/update-user-password.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
 
 @Controller('user')
 export class UserController {
@@ -35,4 +40,65 @@ export class UserController {
 		});
 		return { code: 200, message: '验证码发送成功' };
     }
+
+    // 更新验证码
+    @Get('update_password/captcha')
+	async updatePasswordCaptcha(@Query('address') address: string) {
+		const code = Math.random().toString().slice(2, 8);
+
+		await this.redisService.set(
+			`update_password_captcha_${address}`,
+			code,
+			10 * 60,
+		);
+
+		await this.emailService.sendMail({
+			to: address,
+			subject: '更改密码验证码',
+			html: `<p>你的更改密码验证码是 ${code}</p>`,
+		});
+		return '发送成功';
+	}
+
+
+    // 查询用户新
+    @Get('info')
+    @RequireLogin() // 需要登陆
+    async info(@UserInfo('userId') userId: number) {
+        const user = await this.userService.findUserDetailById(userId);
+
+        // 返回 vo 的格式
+        const vo = new UserDetailVo();
+
+        vo.id = user.id;
+		vo.email = user.email;
+		vo.username = user.username;
+		vo.headPic = user.headPic;
+		vo.phoneNumber = user.phoneNumber;
+		vo.nickName = user.nickName;
+		vo.createTime = user.createTime;
+		vo.isFrozen = user.isFrozen;
+
+        return vo;
+    }
+
+    // 修改密码(管理员和用户修改密码的页面是一样的,用同一个接口)
+    @Post(['update_password', 'admin/update_password'])
+    @RequireLogin()
+    async updatePassword(
+        @UserInfo('userId') userId: number, 
+        @Body() passwordDto: UpdateUserPasswordDto,
+    ) {
+        return await this.userService.updatePassword(userId, passwordDto);
+    }
+
+    // 修改用户信息
+    @Post(['update', 'admin/update'])
+	@RequireLogin()
+	async update(
+		@UserInfo('userId') userId: number,
+		@Body() updateUserDto: UpdateUserDto,
+	) {
+		return await this.userService.update(userId, updateUserDto);
+	}
 }
--- meeting_room_booking_system_backend/src/user/user.service.ts
+++ meeting_room_booking_system_backend/src/user/user.service.ts
@@ -5,6 +5,8 @@ import { RegisterUserDto } from './dto/register.dto';
 import { User } from './entities/user.entity';
 import { RedisService } from 'src/redis/redis.service';
 import { md5 } from 'src/utils/md5';
+import { UpdateUserPasswordDto } from './dto/update-user-password.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
@@ -47,6 +49,83 @@ export class UserService {
             this.logger.error(e, UserService);
             return { code: 0, message: '注册失败' };
         }
+    }
+
+    // 查询用户
+    async findUserDetailById(userId: number) {
+        const user = await this.userRepository.findOne({
+            where: {
+                id: userId,
+            }
+        })
+
+        return user;
+    }
+
+    // 修改密码
+    async updatePassword(userId: number, passwordDto: UpdateUserPasswordDto) {
+        const captcha = await this.redisService.get(
+			`update_password_captcha_${passwordDto.email}`,
+		);
+
+        if (passwordDto.captcha !== captcha) {
+			throw new HttpException('验证码已失效', HttpStatus.BAD_REQUEST);
+		}
+
+        if (!captcha) {
+			throw new HttpException('验证码已失效', HttpStatus.BAD_REQUEST);
+		}
+        
+        // 查找用户信息
+        const foundUser = await this.userRepository.findOneBy({
+			id: userId,
+		});
+        // 新密码
+        foundUser.password = md5(passwordDto.password);
+
+		try {
+            // save 前可以先查询一次
+			await this.userRepository.save(foundUser);
+            // 可以让 token 失效，让用户重新登陆一次
+			return '密碼修改成功';
+		} catch (e) {
+			this.logger.error(e, UserService);
+			return '密码修改失败';
+		}
+    }
+
+    // 更新
+    async update(userId: number, updateUserDto: UpdateUserDto) {
+        const captcha = await this.redisService.get(
+			`update_user_captcha_${updateUserDto.email}`,
+		);
+
+		if (!captcha) {
+			throw new HttpException('验证码已失效', HttpStatus.BAD_REQUEST);
+		}
+
+		if (updateUserDto.captcha !== captcha) {
+			throw new HttpException('验证码不正确', HttpStatus.BAD_REQUEST);
+		}
+
+        const foundUser = await this.userRepository.findOneBy({
+			id: userId,
+		});
+
+		if (updateUserDto.nickName) {
+			foundUser.nickName = updateUserDto.nickName;
+		}
+		if (updateUserDto.headPic) {
+			foundUser.headPic = updateUserDto.headPic;
+		}
+
+        try {
+			await this.userRepository.save(foundUser);
+			return '用户信息修改成功';
+		} catch (e) {
+			this.logger.error(e, UserService);
+			return '用户信息修改成功';
+		}
     }
 }
 
